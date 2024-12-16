22639a466b99b165d4d071cf76be44cce9863100
Adrian <110479078+a7asoft@users.noreply.github.com>
2024-12-16 16:22:42 -0500

    move serviceWorker

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './scss/main.scss'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from '../public/serviceWorker.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
